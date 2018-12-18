61257a1424e390a03bdbe95995de7dce8c0660e2
Justin <justin@baal-software.com>
2018-12-17 17:04:40 -0700

    Add carousel default behaviour

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar/';
-import Featured from './Components/Featured/';
-import Carousel from './Components/Carousel/';
-
+import Navbar from './Components/Navbar';
+import Featured from './Components/Featured';
+import Carousel from './Components/Carousel';
+import Data from './Data'
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {data:Data}
+  }
+
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <Navbar  />
         <Featured />
-        <Carousel />
+        <Carousel data={this.state.data} />
+
       </div>
     );
   }
--- src/Components/Carousel/index.js
+++ src/Components/Carousel/index.js
@@ -1,12 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Carousel.css';
 
 const Carousel = (props) => {
+
   return (
     <nav className="Carousel">
-
+      left
+      <div className="scroll-container">
+      <div className="carousel-game">
+        <span>{props.data.games[0].game}</span>
+      </div>
+      <div className="carousel-game">
+        <span>{props.data.games[1].game}</span>
+      </div>
+      <div className="carousel-game">
+        <span>{props.data.games[2].game}</span>
+      </div>
+      <div className="carousel-game">
+        <span>{props.data.games[3].game}</span>
+      </div>
+      <div className="carousel-game">
+        <span>{props.data.games[4].game}</span>
+      </div>
+      <div className="carousel-game">
+        <span>{props.data.games[0].game}</span>
+      </div>
+      <div className="carousel-game">
+        <span>{props.data.games[0].game}</span>
+      </div>
+      <div className="carousel-game">
+        <span>{props.data.games[0].game}</span>
+        </div>
+        </div>
+      right
     </nav>
   )
-}
+  }
 
 export default Carousel;
