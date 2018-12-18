ca85ea8f5c6b95aae1f26d16f91802e0319500c4
Whitney Burton <whitneyburton9@gmail.com>
2018-12-18 16:28:14 -0700

    Add all game cards to carousels

--- src/Components/Carousel/index.js
+++ src/Components/Carousel/index.js
@@ -5,11 +5,14 @@ const Carousel = (props) => {
   return (
     <nav className="Carousel">
       left
+      <h4>{props.genre.genre}</h4>
       <div className="scroll-container">
-      <div className="carousel-game">
-        <h4>{props.genre.genre}</h4>
-        <span>{props.matchingGames[0].game}</span>
-      </div>
+        {props.matchingGames.map(game => {
+          return <div className="carousel-game">
+              <span>{game.game}</span>
+              <img className="carousel-image" src={require('../../img/test.jpg')}></img>
+            </div>
+        })}
         </div>
       right
     </nav>
