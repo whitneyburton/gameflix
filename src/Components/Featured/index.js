80aa76bbf030e9efe5a147e08f500894f9a3631b
Whitney Burton <whitneyburton9@gmail.com>
2018-12-18 22:57:10 -0700

    Adjust css and add img data

--- src/Components/Featured/index.js
+++ src/Components/Featured/index.js
@@ -4,7 +4,7 @@ import './Featured.css';
 const Featured = (props) => {
   return (
     <nav className="Featured">
-      <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + props.data[1].youtube} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="100%" height="100%" src={"https://www.youtube.com/embed/" + props.data[1].youtube} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </nav>
   )
 }
