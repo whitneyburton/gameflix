36ba7a8b65a422050e45f9eb13bd1ab28fa16c63
Justin <justin@baal-software.com>
2018-12-22 12:24:02 -0700

    Add additional information to popups #2

--- src/Components/Popup/index.js
+++ src/Components/Popup/index.js
@@ -3,14 +3,16 @@ import './PopUp.css';
 
 export default class PopUp extends Component {
   render() {
-    let className="PopUp";
-    if(this.props.isSearch) {
-      className="search-popup"
+    let className = "PopUp";
+    if (this.props.isSearch) {
+      className = "search-popup"
     }
     return (
       <div className={className}>
-      <i onClick={this.props.closePopUp} class="fas fa-times"></i>
-      {this.props.game.game}
+        <i onClick={this.props.closePopUp} class="fas fa-times"></i>
+        <h1>{this.props.game.game}</h1>
+        <p>{this.props.game.description}</p>
+        <img className="carousel-image" src={this.props.game.img} alt="" />
       </div>
     )
   }
