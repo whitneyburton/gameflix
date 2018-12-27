7762ba13c97becf79c15ec6896b61ee3307a5675
Whitney Burton <whitneyburton9@gmail.com>
2018-12-27 14:09:40 -0700

    Add PopUp game details and initial styling

--- src/Components/PopUp.js
+++ src/Components/PopUp.js
@@ -10,11 +10,31 @@ export default class PopUp extends Component {
     }
 
     return (
-      <div className={className}>]
-        <i onClick={closePopUp} className="fas fa-times"></i>
-        <h1>{game.game}</h1>
-        <p>{game.description}</p>
-        <img className="carousel-image" src={game.img} alt="" />
+      <div className={className}>
+        <section>
+          <i onClick={closePopUp} className="fas fa-times"></i>
+          <h1 class="game-name">{game.game}</h1>
+          <div class="game-info">
+            <p class="age-range"><i class="fas fa-birthday-cake"></i> Ages {game.min_age}+</p>
+            <p class="num-of-players"><i class="fas fa-users"></i> {game.min_players} - {game.max_players} players</p>
+            <p class="num-of-minutes"><i class="fas fa-clock"></i> {game.number_of_minutes} min.</p>
+          </div>
+          <p class="game-description">{game.description}</p>
+          
+          <img className="carousel-image" src={game.img} alt="game in box" />
+        </section>
+
+        <section>
+          <iframe
+            width="100%"
+            height="100%"
+            src={`https://www.youtube.com/embed/${game.youtube}?autoplay=0`}
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="featured-video">
+          </iframe>
+        </section>
       </div>
     )
   }
