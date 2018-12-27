be3cb01f4b07a24b1fa11df2ed2504e783b8c1c9
Matthew Foxwell <foxwellmw@gmail.com>
2018-12-27 16:50:07 -0700

    Add logic for carousel scroll btns

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -3,14 +3,20 @@ import GameCard from './GameCard';
 import { uid } from 'react-uid';
 
 class Carousel extends Component {
+
+  shiftCarousel(event) {
+    const shiftDirection = event.target.classList.contains('fa-angle-right') ? 1 : -1;
+    const currentPosition = event.target.parentElement.childNodes[2].scrollLeft;
+    event.target.parentElement.childNodes[2].scrollLeft = currentPosition + (250 * shiftDirection);
+  }
+
   render() {
     const { genre, matchingGames, createPopUp, children } = this.props;
     return (
       <nav className="Carousel" data-genre={genre.genre}>
         <h4 className="genre">{genre.genre}</h4>
-
-        <i className="fas fa-angle-left"></i>
-
+        <i onClick={this.shiftCarousel} className="fas fa-angle-left"></i>
+        
         <div className="scroll-container">
           {matchingGames.map(game => {
             return (
@@ -19,10 +25,10 @@ class Carousel extends Component {
                 game={game}
                 createPopUp={createPopUp} />
             )
-          })}
+          })} 
         </div>
 
-        <i className="fas fa-angle-right"></i>
+        <i onClick={this.shiftCarousel} className="fas fa-angle-right"></i>
         {children}
       </nav>
     )
