32eeb26abc30981fadb823590fc6225a623a4058
Whitney Burton <whitneyburton9@gmail.com>
2018-12-28 17:52:00 -0700

    Add home, board games, and card games links, and styling to navbar and carousels

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,13 +4,21 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav className="Navbar">
-        <i class="fas fa-bars"></i>
-        <input
-          onChange={this.props.checkFilterInput}
-          type="text"
-          className="searchbar"
-          placeholder="Search Here">
-        </input>
+        <div class="hamburger-filters">
+          <i class="fas fa-bars"></i>
+          <p class="home">Home</p>
+          <p class="filter-boardgames">Board Games</p>
+          <p class="filter-cardgames">Card Games</p>
+        </div>
+        <form class="search-field">
+          <i class="fas fa-search"></i>
+          <input
+            onChange={this.props.checkFilterInput}
+            type="text"
+            className="searchbar"
+            placeholder= "Search all games">
+          </input>
+        </form>
       </nav>
     )
   }
