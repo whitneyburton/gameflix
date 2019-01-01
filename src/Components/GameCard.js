078097541f484f01f27b5256694f8b04d43bcec9
Matthew Foxwell <foxwellmw@gmail.com>
2019-01-01 13:34:09 -0700

    Add GameCard scss file

--- src/Components/GameCard.js
+++ src/Components/GameCard.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'
 
 export default class extends Component {
-  
+
   render() {
     const { game, createPopUp } = this.props;
-    
+
     return (
       <div key={game.game} className="carousel-game">
         <div className="game-card" onClick={createPopUp}>
-      
-          <span className="game-card-header">{game.game}</span>
-      
-          <img className="carousel-image" src={game.img} alt="game board"/>
+          <div className="game-card-header-container">
+            <div className="game-card-header-title-container">
+              <span className="game-card-header">{game.game}</span>
+            </div>
+          </div>
+          <img className="game-card-image" src={game.img} alt="game board" />
         </div>
       </div>
     )
