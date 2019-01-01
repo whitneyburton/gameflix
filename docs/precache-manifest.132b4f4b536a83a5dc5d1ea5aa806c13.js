06d742a1b35e6752437aefe3c44c0b901e9a1819
Justin <justin@baal-software.com>
2019-01-01 11:58:16 -0700

    Add gh-pages

---
+++ docs/precache-manifest.132b4f4b536a83a5dc5d1ea5aa806c13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2ec51abb201aaaa3da2b",
+    "url": "/static/js/main.2ec51abb.chunk.js"
+  },
+  {
+    "revision": "45b8b7b5bea536477ad5",
+    "url": "/static/js/1.45b8b7b5.chunk.js"
+  },
+  {
+    "revision": "2ec51abb201aaaa3da2b",
+    "url": "/static/css/main.6de1e5e5.chunk.css"
+  },
+  {
+    "revision": "6020c8b5b5d6eaa96be6a9cfdcbd9a23",
+    "url": "/index.html"
+  }
+];
