e0981bee2057e70224be30f9f1ea472443bcf1b2
Justin <justin@baal-software.com>
2019-01-03 08:46:05 -0700

    See homepage when visit site

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      isHomePage: false,
+      isHomePage: true,
       errors: null,
       games: null,
       genres: null,
