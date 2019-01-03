a524d1dfb6c9bf890bc8769c3672791a45df5e07
Matthew Foxwell <foxwellmw@gmail.com>
2019-01-02 22:02:27 -0700

    Fix adv search transition bug

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      isHomePage: true,
+      isHomePage: false,
       errors: null,
       games: null,
       genres: null,
