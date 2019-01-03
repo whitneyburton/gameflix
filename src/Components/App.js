d9c6e73872b8046a4123aed3802b0a7628a87722
Justin <justin@baal-software.com>
2019-01-02 19:53:50 -0700

    Change value for homescreen

--- src/Components/App.js
+++ src/Components/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      isHomePage: false,
+      isHomePage: true,
       errors: null,
       games: null,
       genres: null,
